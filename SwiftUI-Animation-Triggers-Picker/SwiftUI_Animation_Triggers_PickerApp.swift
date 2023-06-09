20345d99fc0de95c02df933b0dada080074cefd4
ekayaint <ekayaint@gmail.com>
2023-06-09 16:34:53 +0300

    Initial Commit

---
+++ SwiftUI-Animation-Triggers-Picker/SwiftUI_Animation_Triggers_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Animation_Triggers_PickerApp.swift
+//  SwiftUI-Animation-Triggers-Picker
+//
+//  Created by ekayaint on 9.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Animation_Triggers_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
