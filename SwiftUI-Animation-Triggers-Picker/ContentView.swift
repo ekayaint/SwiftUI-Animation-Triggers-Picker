6c63de6633ff0ed7d4b4a5103c7d630f7e816029
ekayaint <ekayaint@gmail.com>
2023-06-10 13:31:38 +0300

    Sample finished

--- SwiftUI-Animation-Triggers-Picker/ContentView.swift
+++ SwiftUI-Animation-Triggers-Picker/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var segment = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack(spacing: 20) {
+            Text("Triggers").font(.largeTitle)
+            Text("Picker").font(.title).foregroundColor(.gray)
+            
+            Picker("", selection: $segment) {
+                Text("Gündüz ☀️").tag(0)
+                Text("Gece 🌙").tag(1)
+            }.pickerStyle(SegmentedPickerStyle())
+                .padding(.horizontal)
+            
+            GeometryReader { gr in
+                ZStack {
+                    VStack(spacing: 20) {
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            Image(systemName: "sun.max.fill").font(.system(size: 50))
+                            Spacer()
+                        }.padding(.top, 25)
+                        Text("Day").fontWeight(.bold)
+                        Spacer()
+                    }.background(Image("sunSea").resizable().aspectRatio(contentMode: .fit))
+                        .cornerRadius(20)
+                        .foregroundColor(.white)
+                        .offset(x: self.segment == 0 ? 0 : -gr.size.width, y: 0)
+                        .animation(.default, value: segment)
+                    
+                    VStack(spacing: 20) {
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            Image(systemName: "moon.fill").font(.system(size: 50))
+                            Spacer()
+                        }.padding(.top, 25)
+                        Text("Night").fontWeight(.bold)
+                        Spacer()
+                    }.background(Image("nightMoon").resizable().aspectRatio(contentMode: .fit))
+                        .cornerRadius(20)
+                        .foregroundColor(.white)
+                        .offset(x: self.segment == 1 ? 0 : gr.size.width, y: 0)
+                        .animation(.default, value: segment)
+                }.shadow(radius: 15)
+                    .padding(.horizontal)
+            }
+            
+        } // VSTACK
+        
     }
 }
 
